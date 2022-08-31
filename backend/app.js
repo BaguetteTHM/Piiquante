6152db2dee085f5bb92e5f6268e6dcf86be3e1cf
baguetteTHM <theohuguesmartin@gmail.com>
2022-08-31 17:42:59 +0200

    res HEADER 2

--- backend/app.js
+++ backend/app.js
@@ -2,25 +2,32 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Schmoo:<ztbfY3WtRF4YrwEz>@cluster0.xgzqkkd.mongodb.net/test',
+mongoose.connect('mongodb+srv://Schmoo:ztbfY3WtRF4YrwEz@cluster0.xgzqkkd.mongodb.net/test',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .then(() => console.log('Connexion à MongoDB réussie '))
+  .catch(() => console.log('Connexion à MongoDB échouée '));
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/api/sauces', (req, res, next) => {
-    delete req.body.userId;
-    const sauce = new Sauce({
-      ...req.body
-    });
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// app.post('/api/sauces', (req, res, next) => {
+//     delete req.body.userId;
+//     const sauce = new Sauce({
+//       ...req.body
+//     });
+//     sauce.save()
+//       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+//       .catch(error => res.status(400).json({ error }));
+//   });
 
 app.get('/api/sauces', (req, res, next) => {
     const sauces = [
