13d2113e5d75333a8e2d1a8c9c2db0bc7e46ec2e
baguetteTHM <theohuguesmartin@gmail.com>
2022-09-19 21:50:28 +0200

    limiter working maybe ?

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
 const userCtrl = require('../controllers/user');
+rateLimiter = require('../middleware/rateLimiter')
+
 
 // route création d'un nouveau user
-router.post('/signup',userCtrl.signup);
+router.post('/signup',rateLimiter.limiter,userCtrl.signup);
 
 // route connexion d'un user existant
-router.post('/login',userCtrl.login);
+router.post('/login',rateLimiter.limiter,userCtrl.login);
 
 
 module.exports = router; //export le router
